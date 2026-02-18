fbb10e71e9b3e073b84d128adc44339c0d9be724
ALEX-beep-code <tatianametreveli@gmail.com>
2026-02-18 20:21:55 +0400

    Updated file

---
+++ day 4.py
@@ -0,0 +1,16 @@
+#input-გამოიყენება მომხმარებელთან კონტაქტისთვის მაგალითად.
+#name = (input)
+name = input("Enter your name")
+print("hello" + " " + name)
+#Output-Output არის ის რასაც input-ში ჩავწერთ.
+Adress=input("Enter your adress")
+hobby=input("tell us your hobby")
+fav_movie=input("your favourite movie")
+surname=input("Enter your surname")
+print(name + "'s" + " " + "favourite movie is" + "-" + fav_movie + " " + "their hobby is" + "-" + hobby + " " + "their surname is" + " " + surname + " " + "and their adress is" + Adress)
+#პითონი არის case_sensitive ენა რაც ნიშნავს იმას რომ ის არჩევს დიდ ასოებს პატარებისგან და სხვადასხვად თვლის მაგალითად
+salary=100
+Salary=150
+print(Salary)
+#snake_case არის _ რომელიც გამოიყენება space-ის მაგივრად და ეს არის სტანდარტი
+yearly_salary=100000
