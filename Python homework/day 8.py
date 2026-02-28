8e37486bbced0b96503205a2ee9ce831115c1b1f
ALEX-beep-code <tatianametreveli@gmail.com>
2026-02-28 15:57:14 +0400

    :updated file

---
+++ Python homework/day 8.py
@@ -0,0 +1,58 @@
+number1=int(input("enter a number"))
+number2 =int(input("enter a number"))
+number3 =int(input("enter a number"))
+number4 =int(input("enter a number"))
+number5 =int(input("enter a number"))
+print(type(number1))
+print(type(number2))
+print(type(number3))
+print(type(number4))
+print(type(number5))
+word1 = str(input())
+word2 = str(input())
+word3 = str(input())
+word4 = str(input())
+word5 = str(input())
+print(type(word1))
+print(type(word2))
+print(type(word3))
+print(type(word4))
+print(type(word5))
+float1 = float(input())
+float2 = float(input())
+float3 = float(input())
+float4 = float(input())
+float5 = float(input())
+print(type(float1))
+print(type(float2))
+print(type(float3))
+print(type(float4))
+print(type(float5))
+
+#მინიჭების ოპერატორი მაგ:
+ten=10
+#მინიჭების ოპერატორი არის =
+
+#მათემატიკური ოპერატორი მუშაობს რიცხვებთან და იყენებს + - * / // **
+print(10 * 10)
+#ამ შემთხვევაში მათემატიკური ოპერატორი * - არის
+
+#შედარების ოპერატორი ასევე მუშობს რიცხვებთან და იყენებს > < => <=
+
+#ლოგიკური ოპერატორი მუშაობს ლოგიკური ტიპის მნიშვნელობებთან
+#bool/boolean = ლოგიკური ტიპის მნიშვნელობა
+#true არის მართალი
+#false ცრუ
+
+
+print(90 > 2)#true
+print(5 < 10)#true
+print(98 <= 123)#true
+print(66 >= 17)#true
+print(25 >= 89)#false
+
+#boolean-დატას ტიპი რომლსაც 2 მნიშვნელობააქ მარტო true/false
+
+number1=int(input("enter a number"))
+number2=int(input("enter a number"))
+print(number1 > number2)
