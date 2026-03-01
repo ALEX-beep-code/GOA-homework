da503e7172703d511d6c973e398ec3263c4588fe
ALEX-beep-code <tatianametreveli@gmail.com>
2026-03-01 15:11:47 +0400

    second commit

--- Python homework/day 8.py
+++ Python homework/day 8.py
@@ -3,26 +3,31 @@ number2 =int(input("enter a number"))
 number3 =int(input("enter a number"))
 number4 =int(input("enter a number"))
 number5 =int(input("enter a number"))
+
 print(type(number1))
 print(type(number2))
 print(type(number3))
 print(type(number4))
 print(type(number5))
+
 word1 = str(input())
 word2 = str(input())
 word3 = str(input())
 word4 = str(input())
 word5 = str(input())
+
 print(type(word1))
 print(type(word2))
 print(type(word3))
 print(type(word4))
 print(type(word5))
+
 float1 = float(input())
 float2 = float(input())
 float3 = float(input())
 float4 = float(input())
 float5 = float(input())
+
 print(type(float1))
 print(type(float2))
 print(type(float3))
@@ -55,4 +60,16 @@ print(25 >= 89)#false
 
 number1=int(input("enter a number"))
 number2=int(input("enter a number"))
+
 print(number1 > number2)
+
+number1=int(input("enter a number"))
+number2=int(input("enter a number"))
+
+print(number1 > 10)
+print(number2 > 20)
+
+number=int(input("enter a number"))
+
+print(number < 100)
+print(number > 999)
