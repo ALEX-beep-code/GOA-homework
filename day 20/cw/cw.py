218d53c1aa7badc93e8ca695f7d11cf50649db8f
ALEX-beep-code <tatianametreveli@gmail.com>
2026-04-10 18:56:13 +0400

    hw

---
+++ day 20/cw/cw.py
@@ -0,0 +1,3 @@
+my_favourite_animals = ["cats" , "dogs"]
+my_school_grades = ["math - 10" , "english - 10"]
+all_types = ["hi" , 12 , "cat" == True , 12.3]
---
+++ day 20/cw/cw2.py
@@ -0,0 +1,6 @@
+my_fav_films = ["avatar" , "avatar fire and ash " , "IT" , "sonic3" , "hello kitty"]
+
+if my_fav_films[4] == "Hello Kitty":
+    my_fav_films[4] = "avatar way of water"
+
+print (my_fav_films)
